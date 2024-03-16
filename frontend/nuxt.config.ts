55ad7b970bbbd9e52acbd258a7d9617533d2edfb
Eshel <eshel@scrtlabs.com>
2024-03-16 05:14:44 +0000

    reverting to previous chain id which is correct, my bad

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -12,7 +12,7 @@ export default defineNuxtConfig({
   modules: ["@pinia/nuxt", "@bootstrap-vue-next/nuxt"],
   runtimeConfig: {
     public: {
-      NETWORK_CHAIN_ID: process.env.NUXT_ENV_NETWORK_CHAIN_ID || "42069",
+      NETWORK_CHAIN_ID: process.env.NUXT_ENV_NETWORK_CHAIN_ID || "412346",
       NETWORK_RPC_URL: process.env.NUXT_ENV_NETWORK_RPC_URL || "http://localhost:42069",
       NETWORK_EXPLORER_URL: process.env.NUXT_ENV_NETWORK_EXPLORER_URL || "https://your.explorer.url.here",
       BACKEND_SERVICE_URL: process.env.NUXT_ENV_BACKEND_SERVICE_URL || "http://localhost:1337",
